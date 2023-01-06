42fda41036613df9600182dc1438ee74bb37a597
Gvantsa Okropiridze <oqropiridzegvantsa@gmail.com>
2023-01-06 20:38:52 +0400

    helper

---
+++ ConsoleApp/Builder/IBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf;
+using ConsoleApp.Model;
+
+namespace ConsoleApp.Builder
+{
+    public abstract class IBuilder
+    {
+        protected Html html;
+
+        public abstract PdfLayoutResult HeaderPoints(string text, float yPosition, PdfPage page);
+        public abstract PdfLayoutResult BodyContent(string text, float yPosition, PdfPage page);
+
+
+        public void CreateNewHtml()
+        {
+            html = new Html();
+        }
+        public Html GetHtml()
+        {
+            return html;
+        }
+    }
+}
---
+++ ConsoleApp/ConcreteBuilder/DynamicConverter.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp.Builder;
+using Syncfusion.Drawing;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+
+namespace ConsoleApp.ConcreteBuilder
+{
+    public class DynamicConverter : IBuilder
+    {
+        //ფერები     
+        Color gray = Color.FromArgb(255, 77, 77, 77);
+        Color black = Color.FromArgb(255, 0, 0, 0);
+        Color white = Color.FromArgb(255, 255, 255, 255);
+        Color violet = Color.FromArgb(255, 151, 108, 174);
+        public override PdfLayoutResult BodyContent(string text, float yPosition, PdfPage page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override PdfLayoutResult HeaderPoints(string text, float yPosition, PdfPage page)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
